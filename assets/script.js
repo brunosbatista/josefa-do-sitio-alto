918a03c5470322f245a3c80736561d97c7417f8d
Bruno Batista <brunosantos.develop@gmail.com>
2024-11-02 14:00:47 -0300

    Criando conexao com whtsapp

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,41 @@ window.addEventListener("scroll", function() {
         navbar.classList.remove("header-scrolled");
     }
 });
+
+const mensagemWhasApp = {
+    nome: null,
+    email: null,
+    celular: null,
+    mensagem: null
+
+}
+
+function getDadosForms() {
+    event.preventDefault()
+    let nome = document.querySelector('#nome').value;
+    let email = document.querySelector('#email').value;
+    let celular = document.querySelector('#telefone').value;
+    let mensagem = document.querySelector('#mensagem').value;
+
+    conexaoApi(nome, email, celular, mensagem);
+    
+}
+
+function conexaoApi(nome, email, celular, mensagem) {
+    enviarMensagem(nome, email, celular, mensagem);
+}
+
+function enviarMensagem(nome, email, celular, mensagem) {
+
+     const numeroWhatsApp = "5579999385306";
+     const texto = `Olá, meu nome é ${nome}.
+        \nEmail: ${email}
+        \nTelefone: ${celular}
+        \nMensagem: ${mensagem}`;
+
+     const textoCodificado = encodeURIComponent(texto);
+
+     const linkWhatsApp = `https://wa.me/${numeroWhatsApp}?text=${textoCodificado}`;
+
+     window.open(linkWhatsApp, "_blank");
+}
